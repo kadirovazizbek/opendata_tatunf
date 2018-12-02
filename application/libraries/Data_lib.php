1d6557c1e8af562bf6d4cb63a68eace1cc20aeed
Allaniyz <a_aleke@inbox.ru>
2018-12-02 10:28:30 +0500

    little change

--- application/libraries/Data_lib.php
+++ application/libraries/Data_lib.php
@@ -150,12 +150,11 @@ class Data_lib{
         $response = json_decode($this->watson($text));
         //print_r($response);
         $response_text = "";
-        $city = "";
-        $price = "";
+        $city = "Tashkent";
+        $price = "expensive";
         if(is_array($response->intents) && $response->intents[0]->intent == "hotel_suggestion"){
             if(is_array($response->entities) && count($response->entities)>0){
-                $city = "Tashkent";
-                $price = "expensive";
+
                 foreach($response->entities as $entity){
                     //print_r($entity);
                     if($entity->entity == 'city'){
